01af19472db89860fc29d971ae16a2c31ab434b5
Thiruvenkatam A <113424127+Thiruvenkatam007@users.noreply.github.com>
2023-02-22 19:20:51 +0530

    Update app_host.py

--- app_host.py
+++ app_host.py
@@ -25,7 +25,7 @@ def run_query(query):
         return cur.fetchall()
 
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from district_map_registering_table;")
 
 # Print results.
 for row in rows:
