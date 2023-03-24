38c95ce2b7119ac5515b7c27bbc0fe36d831dba3
Thiruvenkatam A <113424127+Thiruvenkatam007@users.noreply.github.com>
2023-03-24 12:48:44 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -160,8 +160,10 @@ with geo_analysis:
     fig.update_layout(height=1000, width=1000)
     st.write(' ')
     st.write(' ')
+    import webbrowser
+    chrome = webbrowser.get('chrome')
     if st.button('Click here to see map clearly'):
-        fig.show()
+        fig.show(renderer=chrome)
     st.plotly_chart(fig)
 
 
